aeefddd3c3201be68c01748a978de9948fda517a
ASHLEEN YOUN JI KIM <ashleenkim03@gmail.com>
2019-02-07 05:20:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+add(2,3)
+subtract(60-40)
+multiply(2*3.4)
+divide(5.0/2.5)
+
+parseInt(2,10)
+parseInt('nonsense!', 10)
+parseFloat('80.123999')
+
--- test/index-test.js
+++ test/index-test.js
@@ -5,28 +5,28 @@ beforeEach(function() {
   b = Math.floor(Math.random() * 1000)
 })
 
-it('add(a, b) adds two numbers and returns the result', function() {
+it('add(a, b) adds two numbers and returns the result', function () {
   expect(add(a, b)).toEqual(a + b)
 })
 
-it('subtract(a, b) subtracts b from a and returns the result', function() {
+it('subtract(a, b) subtracts b from a and returns the result', function () {
   expect(subtract(a, b)).toEqual(a - b)
 })
 
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
+it('multiply(a, b) multiplies two numbers and returns the result', function () {
   expect(multiply(a, b)).toEqual(a * b)
 })
 
-it('divide(a, b) divides a by b and returns the result', function() {
+it('divide(a, b) divides a by b and returns the result', function () {
   expect(divide(a, b)).toEqual(a / b)
 })
 
 it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
+  expect(inc(6)).toEqual(6 + 1)
 })
 
 it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
+  expect(dec(8)).toEqual(8 - 1)
 })
 
 describe('makeInt(n)', function() {
